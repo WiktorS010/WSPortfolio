709744b118680ce5cc2217ca9cdd96b3ba2a8682
WiktorS010 <stepienw96@gmail.com>
2024-12-16 19:52:50 +0100

    initialcommit

---
+++ app.js
@@ -0,0 +1,34 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const emailLink = document.querySelector('#copyEmailIcon')
+const email = 'stepienw96@gmail.com';
+const contactLink = document.querySelector('#navbar__links--contact');
+const contactPanel = document.querySelector('.contact__panel');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+emailLink.addEventListener('click', function () {
+
+    navigator.clipboard.writeText(email)
+    .then(() => {
+        alert('Email copied to clipboard');
+    })
+    .catch(err => {
+        console.error('Failed to copy emial: ', err)
+    });
+});
+
+contactLink.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    contactPanel.classList.add('active');
+});
+
+document.addEventListener('click', (e) => {
+    const isClickInside = contactPanel.contains(e.target) || contactLink.contains(e.target);
+    if (!isClickInside) {
+        contactPanel.classList.remove('active');
+    }
+});
